d54466465b9d1a13052221a49e9a22dee1be81ec
bmartel <brandonmartel@gmail.com>
2014-05-23 20:09:26 -0500

    added base adapter and interface for converting data from other class types.

---
+++ src/Bmartel/ArrayTable/Adapters/BaseAdapter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brand_000
+ * Date: 23/05/14
+ * Time: 8:01 PM
+ */
+
+namespace Bmartel\ArrayTable\Adapters;
+
+
+abstract class BaseAdapter implements Convertable {
+
+    protected $dataObj;
+
+} 
---
+++ src/Bmartel/ArrayTable/Adapters/Convertable.php
@@ -0,0 +1,11 @@
+<?php
+namespace Bmartel\ArrayTable\Adapters;
+
+
+interface Convertable {
+
+    /**
+     * @return array
+     */
+    public function convert();
+} 
